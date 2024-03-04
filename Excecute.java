85d5872f90ca256958b381375ba58c0bf111b1f4
Saurabh Mistry <72013349+Saurabh-offixial@users.noreply.github.com>
2024-03-04 08:57:02 +0530

    Add files via upload

---
+++ Excecute.java
@@ -0,0 +1,19 @@
+// Single Inheeritance Program.
+
+import java.util.*;
+
+class Employee 
+{
+    float salary=35000*12;
+}
+
+class Excecute extends Employee
+{
+    float bonus=4500*6;
+    public static void main(String args[])
+    {
+        Excecute obj = new Excecute();
+        System.out.println("Salary is:"+obj.salary);
+        System.out.println("Bonus is:"+obj.bonus);
+    }
+}
